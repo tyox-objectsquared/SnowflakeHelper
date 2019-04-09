c4bf2e84b5ac66faf6f4d81294fa0c7971b3ea28
Tyler Yox <tyox@objectsquared.com>
2019-04-09 14:37:05 -0400

    refactored API, extending token interval

--- snowflake_alerts/app.py
+++ snowflake_alerts/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request, make_response
+from flask import Flask, request, make_response
 from flask_cors import CORS
 import snowflake_access as sa
 import json
@@ -10,8 +10,6 @@ import jwt
 app = Flask(__name__)
 SECRET_KEY = '3GAbKNF938Vq5LZA6TU7jc5zPts2PcA6R6tvehgC3jaLZmdPygJ7CRFPc4rYJnZB'
 app.config['SECRET_KEY'] = SECRET_KEY
-SESSION_TYPE = 'filesystem'
-app.config.from_object(__name__)
 CORS(app)
 
 # TODO Dockerize
@@ -29,86 +27,62 @@ PILOT_ACCOUNT = 'oz95710.us-east-1'
 
 ### PUBLIC ENDPOINTS - anyone can access ###
 
-@app.route('/coops', methods=['GET'])
-def coops():
-    sao = sa.SnowflakeAccess(ADMIN_USERNAME, ADMIN_PASSWORD, ADMIN_ACCOUNT)
-    coops = sao.coops(COOPS_TABLE_NAME, ADMIN_WH_NAME)
-    if MONITOR_SEDC:
-        coops['SEDC'] = 'OZ95710'
-    return json.dumps(coops, default=str)
-
 @app.route('/login',  methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         try:
             data = json.loads(request.data)
-            data[2] += '.us-east-1'
+            data['account'] = data['account'].upper() + '.us-east-1'
             # check if a connection to snowflake can be made
-            sao = sa.SnowflakeAccess(data[0], data[1], data[2])
+            sao = sa.SnowflakeAccess(data['username'], data['password'], data['account'])
             is_authorized = hasattr(sao,'connection')
             if is_authorized:
-                auth_token = encode_auth_token(data[0])
-                if auth_token:
-                    return json.dumps({"isAuth": True, "auth_token": decode_auth_token(auth_token)})
+                auth_token = encode_auth_token(data['username'])
+                if auth_token: #return the encoded auth_token
+                    return make_response(json.dumps({"isAuth": True, "auth_token": auth_token}, default=str), 200)
         except snowflake.connector.errors.DatabaseError as e0:
-            return json.dumps({'isAuth': False, 'message': e0.msg})
+            return make_response(json.dumps({'isAuth': False, 'message': e0.msg}), 200)
 
 
 
 ### PRIVATE ENDPOINTS - authorization header is required ###
 
-@app.route('/metering', methods=['GET'])
-def metering_history():
-    auth_header = request.headers.get('Authorization')
-    if auth_header:
-        resp = decode_auth_token(auth_header)
+def private_request(req, data_method):
+    auth_token = req.headers.get('Authorization')
+    if auth_token:
+        resp = decode_auth_token(auth_token)
         if resp == 'expired': return 'authorization token expired', 401
         if resp == 'invalid': return 'authorization token invalid', 401
-        sao = sa.SnowflakeAccess(PILOT_USERNAME, PILOT_PASSWORD, PILOT_ACCOUNT)
-        return json.dumps(sao.metering_history(), default=str), 200
+        extended_token = extend_auth_token(auth_token)
+        response = make_response(json.dumps(data_method), 200)
+        response.headers['auth_token'] = extended_token
+        return response
     else: #no header
         return 'no authorization provided', 401
+
+
+@app.route('/metering', methods=['GET'])
+def usage_history():
+    sao = sa.SnowflakeAccess(PILOT_USERNAME, PILOT_PASSWORD, PILOT_ACCOUNT)
+    return private_request(request, sao.metering_history())
 
 @app.route('/queries', methods=['GET'])
 def query_history():
-    auth_header = request.headers.get('Authorization')
-    if auth_header:
-        resp = decode_auth_token(auth_header)
-        if resp == 'expired': return 'authorization token expired', 401
-        if resp == 'invalid': return 'authorization token invalid', 401
-        args = {}
-        num_mins = request.args.get('minutes')
-        if num_mins: args['num_minutes'] = num_mins
-        show_ongoing = request.args.get('show_ongoing')
-        if show_ongoing: args['ongoing_only'] = show_ongoing
-        sao = sa.SnowflakeAccess(PILOT_USERNAME, PILOT_PASSWORD, PILOT_ACCOUNT)
-        return json.dumps(sao.query_user_history(**args), default=str), 200
-    else: #no header
-        return 'no authorization provided', 401
+    sao = sa.SnowflakeAccess(PILOT_USERNAME, PILOT_PASSWORD, PILOT_ACCOUNT)
+    return private_request(request, sao.query_user_history(**request.args))
 
 @app.route('/queries/stop/<id>', methods=['POST', 'GET'])
 def stop_query(id):
-    auth_header = request.headers.get('Authorization')
-    if auth_header:
-        resp = decode_auth_token(auth_header)
-        if resp == 'expired': return 'authorization token expired', 401
-        if resp == 'invalid': return 'authorization token invalid', 401
-        sao = sa.SnowflakeAccess(PILOT_USERNAME, PILOT_PASSWORD, PILOT_ACCOUNT)
-        return json.dumps(sao.stop_query(id), default=str), 200
-    else: #no header
-        return 'no authorization provided', 401
+    sao = sa.SnowflakeAccess(PILOT_USERNAME, PILOT_PASSWORD, PILOT_ACCOUNT)
+    return private_request(request, sao.stop_query(id))
 
 
 #Helper Methods for jwt
 
 def encode_auth_token(user_id):
-    """
-    Generates the Auth Token
-    :return: string
-    """
     try:
         payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
             'iat': datetime.datetime.utcnow(),
             'sub': user_id
         }
@@ -116,24 +90,33 @@ def encode_auth_token(user_id):
             payload,
             app.config.get('SECRET_KEY'),
             algorithm='HS256'
-        )
+        ).decode('utf-8')
     except Exception as e:
         return e
 
 
 def decode_auth_token(auth_token):
-    """
-    Decodes the auth token
-    :param auth_token:
-    :return: integer|string
-    """
     try:
-        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'), algorithms=['HS256'])
         return payload['sub']
     except jwt.ExpiredSignatureError:
         return 'expired'
     except jwt.InvalidTokenError:
         return 'invalid'
+
+
+def extend_auth_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'), algorithms=['HS256'])
+        payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        ).decode('utf-8')
+    except Exception as e:
+        return e
+
 
 if __name__ == '__main__':
     app.run(debug=True)
